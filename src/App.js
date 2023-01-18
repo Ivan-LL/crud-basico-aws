717a3d39340ccc9ca53b0d06cc2a3b27ad5d1e40
Ivan-LL <ivan.llanos2001@alumnos.ubiobio.cl>
2023-01-18 16:57:35 -0300

    Hola mundo react inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Obj1 from './component/Obj1';
+import { Fragment } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <Fragment>
+    <div>
+      <h1>Hola mundo.</h1>
+      <Obj1 nombre = {"ivan"} apellido={"llanos"} />
     </div>
+    
+  </Fragment>
   );
 }
+
+
 
 export default App;
---
+++ src/component/Obj1.jsx
@@ -0,0 +1,9 @@
+const Obj1 = ({nombre,apellido}) => {
+    return (
+        <div>
+            <h1>{nombre}</h1>
+            <h2>{apellido}</h2>
+        </div>
+    );
+};
+export default Obj1;
