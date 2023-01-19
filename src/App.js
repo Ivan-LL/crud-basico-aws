c18d8e0cdcfa3aa83b9f3586e07e316a787587cc
Ivan-LL <ivan.llanos2001@alumnos.ubiobio.cl>
2023-01-19 19:01:40 -0300

    Iniciando react-router

--- src/App.js
+++ src/App.js
@@ -2,16 +2,28 @@ import Obj1 from './component/Obj1';
 import { Component, Fragment } from 'react';
 import AContainer from './containers/AContainer';
 import Home from './component/Home'
+import{BrowserRouter, Route, Routes} from "react-router-dom";
 
 class App extends Component {
   render(){
     return (
-      <Fragment>
-        <Home />
-        
-        <hr></hr>
-        <AContainer />
-      </Fragment>
+      <BrowserRouter>
+        <Routes>  
+          <Route path="/" element = {
+            <Fragment>
+              <Home />
+
+                <hr></hr>
+              <AContainer />
+            </Fragment>
+          }/>
+          <Route path="/listar_todo" element = {
+            <div>
+              <h1>LISTANDO TODO</h1>
+            </div>
+          }/>
+        </Routes>
+      </BrowserRouter>
     );
   }
 }
