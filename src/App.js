63c6d0e43bc8241789a63a2a9518296b198396a2
Sebastian Gasc <sebastian.gasc1701@alumno.ubiobio.cl>
2023-01-19 18:26:02 -0300

    Creado componente home

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import Obj1 from './component/Obj1';
 import { Component, Fragment } from 'react';
 import AContainer from './containers/AContainer';
+import Home from './component/Home'
 
 class App extends Component {
   render(){
     return (
       <Fragment>
-        <div>
-          <h1>Hola mundo.</h1>
-          <Obj1 nombre = {"ivan"} apellido={"llanos"} />
-          <AContainer />
-        </div>
+        <Home />
+        
+        <hr></hr>
+        <AContainer />
       </Fragment>
     );
   }
---
+++ src/component/Home.js
@@ -0,0 +1,25 @@
+const Home=()=>{
+    return(
+        <div>
+            <div>
+              <button >Agregar</button>
+              <input type="text" placeholder='Agregar'/>
+            </div>
+
+            <div>
+              <button >Eliminar</button>
+              <input type="text" placeholder='Eliminar'/>
+            </div>
+
+            <div>
+              <button >Editar</button>
+              <input type="text" placeholder='Editar'/>
+            </div>
+
+        </div>
+
+    );
+}
+
+
+export default Home;
