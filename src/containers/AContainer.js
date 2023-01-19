b0998e27d3289cef93ef0244c4f8b30a6de4ef4e
Sebastian Gasc <sebastian.gasc1701@alumno.ubiobio.cl>
2023-01-19 12:24:51 -0300

    Mostrar productos en pagina

--- src/containers/AContainer.js
+++ src/containers/AContainer.js
@@ -1,4 +1,4 @@
-import react, {Component} from 'react';
+import react, {Component, Fragment} from 'react';
 import axios from 'axios';
 import{
     BrowserRouter as Router,
@@ -7,18 +7,50 @@ import{
     Link
 } from "react-router-dom";
 
+
+
 class AContainer extends Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading: true,
+            productos: [],
+        };
+    }
+    
     componentDidMount(){
         axios.get('https://hmftk6fuch.execute-api.us-east-1.amazonaws.com/prod/productos')
         .then(result=>{
-            console.log(result)
-        }).catch(console.log)
+            if(result.data){
+                
+                this.setState({productos:result.data.productos,isLoading:false})
+                console.log(result)
+            }
+        })
     }
 
     render(){
         return(
-            <h1>AWS App</h1>
-        );
+            <div>
+                {
+                    this.state.isLoading ?
+                        "cargando"
+                    :
+                        this.state.productos.map(
+                            (producto,index)=>{
+                                return(
+                                    <div key={index}>
+                                        <p>
+                                            {producto.IdProducto+" "+producto.color+" "+producto.precio}
+                                        </p>
+                                    </div>    
+                                )
+                            }
+                        )
+                }
+            </div>
+        )
     }
 }
 export default AContainer;
