f91e44ebed86af81077c2e793252e2a30e37c6b1
Sebastian Gasc <sebastian.gasc1701@alumno.ubiobio.cl>
2023-01-22 16:52:32 -0300

    Agregada funcionalidad a los inputs

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,31 +1,53 @@
+import React, {useRef} from "react";
+
 const Home=()=>{
 
-    
-    
+    let addId=useRef();
+    let addNombre=useRef();
+    let addPrecio=useRef();
+    let addColor=useRef();
+  
+    const botonAgregar=()=>{
+      var idProducto = addId.current.value
+      var nombreProducto = addNombre.current.value
+      var precioProducto = addPrecio.current.value
+      var colorProducto = addColor.current.value
+
+      console.log(idProducto)
+      console.log(nombreProducto)
+      console.log(precioProducto)
+      console.log(colorProducto)
+    }
+
+    let deleteId=useRef();
+
+
+    let editId=useRef();
+    let editNombre=useRef();
+    let editPrecio=useRef();
+    let editColor=useRef();
+
     return(
         <div>
             <div>
-              <button style={{position:'absolute'}} >Agregar</button>
-              <input type="text" placeholder='id' style={{position:'absolute',top:'10px',left:'80px'}}/>
-              <input type="text" placeholder='Nombre' style={{position:'absolute',top:'30px',left:'80px'}}/>
-              <input type="text" placeholder='precio' style={{position:'absolute',top:'50px',left:'80px'}}/>
-              <input type="text" placeholder='color' style={{position:'absolute',top:'70px',left:'80px'}}/>
+              <button style={{position:'absolute'}} onClick={botonAgregar} >Agregar</button>
+              <input ref={addId} type="text" placeholder='idAgregar' style={{position:'absolute',top:'10px',left:'80px'}}/>
+              <input ref={addNombre} type="text" placeholder='NombreAgregar' style={{position:'absolute',top:'30px',left:'80px'}}/>
+              <input ref={addPrecio} type="text" placeholder='precioAgregar' style={{position:'absolute',top:'50px',left:'80px'}}/>
+              <input ref={addColor} type="text" placeholder='colorAgregar' style={{position:'absolute',top:'70px',left:'80px'}}/>
             </div>
 
             <div>
               <button style={{position:'absolute',top:'110px'}}>Eliminar</button>
-              <input type="text" placeholder='id'  style={{position:'absolute',top:'110px',left:'80px'}}/>
-              <input type="text" placeholder='Nombre' style={{position:'absolute',top:'130px',left:'80px'}}/>
-              <input type="text" placeholder='precio' style={{position:'absolute',top:'150px',left:'80px'}}/>
-              <input type="text" placeholder='color' style={{position:'absolute',top:'170px',left:'80px'}}/>
+              <input ref={deleteId} type="text" placeholder='idEliminar'  style={{position:'absolute',top:'110px',left:'80px'}}/>
             </div>
 
             <div>
               <button style={{position:'absolute',left:'350px'}}>Editar</button>
-              <input type="text" placeholder='id' style={{position:'absolute',top:'10px', left:'410px'}}/>
-              <input type="text" placeholder='Nombre' style={{position:'absolute',top:'30px',left:'410px'}}/>
-              <input type="text" placeholder='precio' style={{position:'absolute',top:'50px', left:'410px'}}/>
-              <input type="text" placeholder='color' style={{position:'absolute',top:'70px', left:'410px'}}/>
+              <input ref={editId} type="text" placeholder='id' style={{position:'absolute',top:'10px', left:'410px'}}/>
+              <input ref={editNombre} type="text" placeholder='Nombre' style={{position:'absolute',top:'30px',left:'410px'}}/>
+              <input ref={editPrecio} type="text" placeholder='precio' style={{position:'absolute',top:'50px', left:'410px'}}/>
+              <input ref={editColor} type="text" placeholder='color' style={{position:'absolute',top:'70px', left:'410px'}}/>
               
             </div>
 
