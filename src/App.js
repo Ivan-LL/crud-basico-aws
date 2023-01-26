3c8b7b02ccd05d59333027c29bc567bc1d969c97
Sebastian Gasc <sebastian.gasc1701@alumno.ubiobio.cl>
2023-01-26 11:07:31 -0300

    Creada home de la pagina

--- src/App.js
+++ src/App.js
@@ -28,6 +28,8 @@ class App extends Component {
           
           <Routes>
 
+          <Route path="/" element={<Home/>}/> 
+
           <Route path="listar_todo" element={<AContainer/>}>
               <Route path="BotonAgregar" element={<BotonAgregar/>}/>
               <Route path="BotonEliminar" element={<BotonEliminar/>}/>
