6f541c32f8c23627693ce33f26c0f8cb28b4060c
Ivan-LL <ivan.llanos2001@alumnos.ubiobio.cl>
2023-01-26 11:27:43 -0300

    Creando ruta informacion

---
+++ src/component/Informacion.js
@@ -0,0 +1,23 @@
+import React, {useRef} from "react";
+import { Placeholder } from "react-bootstrap";
+import ReactiveButton from 'reactive-button';
+const Informacion = () =>{
+
+    return(
+
+        <div style={{position:'absolute',top:'225px',width:'100%'}}>
+            <h3 style={{textAlign:'center'}}>¡Su opinión es importante para nosotros, por favor, contáctenos!</h3>
+            <h1 style={{position:'absolute',top:'205px',left:'680px', fontSize:'28px'}}>Nombre</h1>
+            <h1 style={{position:'absolute',top:'256px',left:'700px', fontSize:'28px'}}>e-mail</h1>
+            <h1 style={{position:'absolute',top:'308px',left:'675px', fontSize:'28px'}}>Mensaje</h1>
+            <input ref={campo_nombre} style={{position:'absolute',top:'230px',left:'790px'}}></input>
+            <input ref={campo_mail} style={{position:'absolute',top:'280px',left:'790px'}}></input>
+            <textarea ref={campo_msg} style={{position:'absolute',top:'330px',left:'790px'}} id="zonamsg" name="zonamsg" rows="6" cols="50">
+            </textarea>
+
+            <ReactiveButton style={{position:'relative',top:'425px',left:'925px', width:'100'}}  size='little' color="dark" idleText="Enviar" onClick={respuesta}/>
+        </div>
+    )
+}
+
+export default Informacion;
