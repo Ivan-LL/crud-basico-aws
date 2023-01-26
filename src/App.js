634d88911b65229611f294cdd89fb5e155a5a27b
Ivan-LL <ivan.llanos2001@alumnos.ubiobio.cl>
2023-01-26 11:30:08 -0300

    creando ruta informacion 2

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import AContainer from './containers/AContainer';
 import ReactiveButton from 'reactive-button';
 import Home from './component/Home'
 import Contacto from './component/Contacto'
+import Informacion from './component/Informacion'
 import BotonEditar from './component/BotonEditar'
 import BotonEliminar from './component/BotonEliminar'
 import BotonAgregar from './component/BotonAgregar'
 import{BrowserRouter, Route, Routes,Link} from "react-router-dom";
 import { Button } from "react-bootstrap"
-//path="/listar_todo" element =
+
 class App extends Component {
   render(){
     return (
@@ -22,19 +23,20 @@ class App extends Component {
           <div style={{"height":"70.844px","width":"100%","background-color":"#000000","display": "flex","align-items":"center","justify-content":'space-evenly'}}>
           <a href="/listar_todo"><ReactiveButton size="small" color="dark" idleText="Lista productos" /></a>
           <a href="/contacto"><ReactiveButton size="small" color="dark" idleText="Contacto"/></a>
-          <ReactiveButton size="small" color="dark" idleText="Información"/>
+          <a href="/informacion"><ReactiveButton size="small" color="dark" idleText="Información"/></a>
           </div>
           <BrowserRouter>
           
           <Routes>
 
           <Route path="listar_todo" element={<AContainer/>}>
-              <Route path="BotonAgregar" element={<BotonAgregar/>}/>
-              <Route path="BotonEliminar" element={<BotonEliminar/>}/>
-              <Route path="BotonEditar" element={<BotonEditar/>}/>
+            <Route path="BotonAgregar" element={<BotonAgregar/>}/>
+            <Route path="BotonEliminar" element={<BotonEliminar/>}/>
+            <Route path="BotonEditar" element={<BotonEditar/>}/>
           </Route>
 
-          <Route path="contacto" element={<Contacto/>}/>
+            <Route path="contacto" element={<Contacto/>}/>
+            <Route path="informacion" element={<Informacion/>}/>
           
 
           </Routes>
